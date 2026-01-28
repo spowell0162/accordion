caaeaaab0cbd277058a457de3997549564e4cb6f
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-28 10:23:52 +0000

    rotating icon

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
 
+// HTML Elements
 const accordionItem = document.querySelectorAll('.accordion_item');
 
+// Event Handlers
 accordionItem.forEach(item => {
     const toggle = item.querySelector('.accordion_toggle');
     const content = item.querySelector('.accordion_content');
+    const icon = item.querySelector('.accordion_icon');
     toggle.addEventListener('click', function () {
         content.classList.toggle('open');
+        icon.classList.toggle('open');
     });
 });
