4005466e96fdd5dd3a15ef37db7660a15952499e
samuelpowellweb <samuelpowellweb@gmail.com>
2026-01-28 15:52:21 +0000

    aria-expanded and focus states

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 // Manually open and close
 document.querySelector('.accordion_component').addEventListener('click', function (e) {
-    const toggle = e.target.closest('.accordion_toggle');
-    if (!toggle) return;
+    const toggle = e.target.closest('.accordion_toggle'); // goes up the dom tree until it finds this class
+    if (!toggle) return; // exits of it can't find the class
     const item = toggle.closest('.accordion_item');
-    item.classList.toggle('open');
+    const isOpen = item.classList.toggle('open'); // .toggle returns boolean - true if added, false if removed
+    toggle.setAttribute('aria-expanded', isOpen); //setAttribute takes the attribute and its value
 });
